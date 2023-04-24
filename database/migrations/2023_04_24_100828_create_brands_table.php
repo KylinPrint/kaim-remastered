2c7301c24f295f917a7ec658fb29fcccf05cfe10
cyanogenic <zhouziqi@ityut.com>
2023-04-24 10:45:35 +0800

    Brand

---
+++ database/migrations/2023_04_24_100828_create_brands_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique()->comment('品牌名称');
+            $table->string('subname')->unique()->nullable()->comment('品牌副名称');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brands');
+    }
+}
